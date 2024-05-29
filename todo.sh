1059803015fdc7242d6403ad5c0efbc6b914dd73
guoyi <1900013009@pku.edu.cn>
2024-05-29 21:23:03 +0800

    update

--- todo.sh
+++ todo.sh
@@ -1,9 +1,9 @@
 # python train.py  --cfg ./configs/resnet50.json --angle_type 0 --split_name 1
-# python train.py  --cfg ./configs/resnet50.json --angle_type 0 --split_name 2
-# python train.py  --cfg ./configs/resnet50.json --angle_type 0 --split_name 3
-# python train.py  --cfg ./configs/resnet50.json --angle_type 0 --split_name 4
+python train.py  --cfg ./configs/resnet50.json --angle_type 0 --split_name 2
+python train.py  --cfg ./configs/resnet50.json --angle_type 0 --split_name 3
+python train.py  --cfg ./configs/resnet50.json --angle_type 0 --split_name 4
 
-python test.py  --cfg ./configs/resnet50.json --angle_type 0 --split_name 1
+# python test.py  --cfg ./configs/resnet50.json --angle_type 0 --split_name 1
 python test.py  --cfg ./configs/resnet50.json --angle_type 0 --split_name 2
 python test.py  --cfg ./configs/resnet50.json --angle_type 0 --split_name 3
 python test.py  --cfg ./configs/resnet50.json --angle_type 0 --split_name 4
